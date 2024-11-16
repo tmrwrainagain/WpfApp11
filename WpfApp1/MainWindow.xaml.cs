b8c4b195936d55f20ac955b430c6e7c106946263
Мирный Алексей <tmrwrainagain@stud.vvsu.ru>
2024-11-16 15:14:39 +1000

    Добавьте файлы проекта.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            { Filter = "Image Files| *.jpg; *.jpeg; *.jpg;" };
+            if (openFileDialog.ShowDialog() == true)
+            {
+                BitmapImage bitmapImage =
+                new BitmapImage(new Uri(openFileDialog.FileName));
+                DisplayImageForm.Source = bitmapImage;
+            }
+            else
+            {
+                MessageBox.Show("Не выбрана картинка");
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window2 window = new Window2();
+            window.Show();
+            this.Close();
+        }
+    }
+
+}
---
+++ WpfApp1/Window2.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для Wi
+    /// ndow2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        private string _pass = "12345678";
+        private string _inputpass = ""; 
+        public Window2()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            _inputpass = pswdTxt.Password;
+            if (_inputpass == _pass)
+            {
+                txtDisplay.Text = "Welcome!";
+
+            }
+            else
+            {
+                MessageBox.Show("Wrong pass!");
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MainWindow window = new MainWindow();
+            window.Show();
+            this.Close();
+        }
+    }
+}
